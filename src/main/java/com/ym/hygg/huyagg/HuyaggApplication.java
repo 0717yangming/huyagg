25795634d8cc486adc4c2e43903540db3536f813
yangming <8480898098@qq.com>
2020-02-14 00:10:23 +0800

    'springboot和boopstrap入门'

---
+++ src/main/java/com/ym/hygg/huyagg/HuyaggApplication.java
@@ -0,0 +1,13 @@
+package com.ym.hygg.huyagg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuyaggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuyaggApplication.class, args);
+    }
+
+}
